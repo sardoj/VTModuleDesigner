ee2022c17dbaafad323e0e55c63990c4c71f879a
Jonathan SARDO <sardoj@gmail.com>
2018-01-19 00:22:14 +0100

    Packages install and config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgDragDropModule } from 'ng-drag-drop';
+
+import {
+  MatTabsModule,
+  MatCardModule,
+  MatInputModule,
+  MatButtonModule,
+  MatIconModule
+} from '@angular/material';
 
 
 import { AppComponent } from './app.component';
+import { LocalePopupComponent } from './locale-popup/locale-popup.component';
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LocalePopupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    NgDragDropModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: HttpLoaderFactory,
+          deps: [HttpClient]
+      }
+    }),
+    // Material components
+    MatTabsModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  entryComponents: [
+    LocalePopupComponent
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
